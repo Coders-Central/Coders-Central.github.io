1db6ae651e41c8079da09599e08fc58965fccd51
M_H <mowz971@gmail.com>
2014-07-05 16:20:39 +0100

    Added database sign-in section

--- js/main.js
+++ js/main.js
@@ -7,3 +7,35 @@ $(document).ready(function(){
 		$('nav').slideToggle('slow');
 	});
 });
+
+//Database code:
+
+Parse.initialize("ivRkO4ESdsZ7LS03mBbux8cQseV4hlKCUC9vnNlX", "KOJOvChPgm4I06kGcb7rjsu45S9BP8oedZWheMld");
+
+$("#submit").click(function() {
+
+        var username = $("#username").val();
+        var password = $("#userpassword").val();
+
+        console.log(username +' '+ password);
+    
+        Parse.User.logIn(username, password, {
+          success: function(user) {
+          	alert('Logged In!');
+            console.log('success');          
+          },
+
+          error: function(user, error) {
+            /*function error() {
+                $('input').addClass('error');
+
+                $('input').click(function(){
+                    $(this).removeClass('error');
+                });
+            };*/
+            //error();
+            console.log('failed!');
+            alert('Error!');
+          }
+        });
+    });
